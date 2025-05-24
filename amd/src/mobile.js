2cddbcf1a797ab05204ef631b69d64712a8e78ce
prevail90 <90661674+prevail90@users.noreply.github.com>
2025-05-24 10:54:09 +0000

    Update mobile.js

--- amd/src/mobile.js
+++ amd/src/mobile.js
@@ -1,76 +1,154 @@
-define(['jquery', 'core/ajax', 'core/notification', 'core/str'], function($, ajax, notification, str) {
+define([
+    'jquery',
+    'core/ajax',
+    'core/templates',
+    'core/notification',
+    'core/str'
+], function($, ajax, templates, notification, str) {
     return {
         init: function() {
-            var self = this;
-
-            // Handle evaluation form submission
-            $('#mod-customeval-evaluation-form').on('submit', function(e) {
-                e.preventDefault();
-                self.submitEvaluation($(this));
-            });
-
-            // Initialize offline sync if in mobile app
-            if (typeof M !== 'undefined' && M.mod_customeval) {
-                self.initMobileOfflineSupport();
-            }
+            this.setupEventListeners();
+            this.loadInitialData();
         },
 
-        submitEvaluation: function(form) {
-            var promises = [];
-            var formData = form.serializeArray();
-            var cmid = form.find('input[name="cmid"]').val();
-            var userid = form.find('input[name="userid"]').val();
+        setupEventListeners: function() {
+            // Delegated event for student selection
+            $(document).on('click', '.student-list-item', (e) => this.loadStudentEvaluation(e));
+            
+            // Delegated event for form submission
+            $(document).on('submit', '#mod-customeval-mobile-form', (e) => this.handleFormSubmit(e));
+        },
 
-            // Prepare strings for notifications
-            promises.push(str.get_string('evaluationsaved', 'mod_customeval'));
-            promises.push(str.get_string('error:submissionfailed', 'mod_customeval'));
+        loadInitialData: function() {
+            const container = $('#mod-customeval-mobile-container');
+            const cmid = container.data('cmid');
+            const courseid = container.data('courseid');
 
-            // Submit via AJAX
-            promises.push(ajax.call([{
-                methodname: 'mod_customeval_submit_evaluation',
-                args: {
+            ajax.call([{
+                methodname: 'mod_customeval_get_mobile_data',
+                args: { 
                     cmid: cmid,
-                    userid: userid,
-                    formdata: formData
-                }
-            }]));
-
-            $.when.apply($, promises).done(function(successMsg, errorMsg, response) {
-                if (response[0].success) {
-                    notification.alert(successMsg, response[0].message, 'success');
-                } else {
-                    notification.alert(errorMsg, response[0].message, 'error');
-                }
-            }).fail(notification.exception);
-        },
-
-        initMobileOfflineSupport: function() {
-            // Register offline handlers
-            M.mod_customeval = M.mod_customeval || {};
-            M.mod_customeval.offline = {
-                cacheEvaluations: function() {
-                    // Store evaluations in mobile app cache
+                    courseid: courseid
                 },
-                syncPending: function() {
-                    // Sync pending evaluations when online
+                done: (response) => {
+                    this.renderActivityView(response);
+                },
+                fail: (error) => {
+                    notification.exception(error);
+                    this.showErrorMessage('loaderror');
                 }
-            };
-
-            // Register with Moodle Mobile
-            if (M.moodleMobile) {
-                M.moodleMobile.registerModule('mod_customeval', {
-                    sync: this.syncHandler
-                });
-            }
+            }]);
         },
 
-        syncHandler: function() {
-            return {
-                execute: function() {
-                    // Implement sync logic here
-                    return $.Deferred().resolve();
+        renderActivityView: function(response) {
+            templates.render('mod_customeval/mobile_view_activity', response)
+                .then((html) => {
+                    $('#mod-customeval-mobile-container').html(html);
+                    return str.get_string('activityloaded', 'mod_customeval');
+                })
+                .then((message) => {
+                    notification.addNotification({
+                        message: message,
+                        type: 'success'
+                    });
+                })
+                .catch((error) => {
+                    notification.exception(error);
+                    this.showErrorMessage('rendererror');
+                });
+        },
+
+        loadStudentEvaluation: function(e) {
+            const studentId = $(e.currentTarget).data('userid');
+            const cmid = $('#mod-customeval-mobile-container').data('cmid');
+
+            ajax.call([{
+                methodname: 'mod_customeval_get_evaluation_form',
+                args: {
+                    userid: studentId,
+                    cmid: cmid
+                },
+                done: (response) => {
+                    this.renderEvaluationForm(response);
+                },
+                fail: (error) => {
+                    notification.exception(error);
+                    this.showErrorMessage('loaderror');
                 }
-            };
+            }]);
+        },
+
+        renderEvaluationForm: function(response) {
+            templates.render('mod_customeval/mobile_evaluation_form', response)
+                .then((html) => {
+                    $('#evaluation-container').html(html);
+                    return str.get_string('formloaded', 'mod_customeval');
+                })
+                .then((message) => {
+                    notification.addNotification({
+                        message: message,
+                        type: 'success'
+                    });
+                })
+                .catch((error) => {
+                    notification.exception(error);
+                    this.showErrorMessage('rendererror');
+                });
+        },
+
+        handleFormSubmit: function(e) {
+            e.preventDefault();
+            const form = $(e.target);
+            const formData = form.serialize();
+
+            str.get_strings([
+                { key: 'saving', component: 'mod_customeval' },
+                { key: 'savesuccess', component: 'mod_customeval' },
+                { key: 'saveerror', component: 'mod_customeval' }
+            ]).then((strings) => {
+                notification.addNotification({
+                    message: strings[0],
+                    type: 'info'
+                });
+
+                ajax.call([{
+                    methodname: 'mod_customeval_submit_evaluation',
+                    args: formData,
+                    done: (response) => {
+                        notification.addNotification({
+                            message: strings[1],
+                            type: 'success'
+                        });
+                        this.loadInitialData(); // Refresh the list
+                    },
+                    fail: (error) => {
+                        notification.exception(error);
+                        notification.addNotification({
+                            message: strings[2],
+                            type: 'error'
+                        });
+                    }
+                }]);
+            }).catch((error) => {
+                notification.exception(error);
+                this.showErrorMessage('genericerror');
+            });
+        },
+
+        showErrorMessage: function(errorKey) {
+            str.get_string(errorKey, 'mod_customeval')
+                .then((message) => {
+                    notification.addNotification({
+                        message: message,
+                        type: 'error'
+                    });
+                })
+                .catch(() => {
+                    notification.addNotification({
+                        message: 'An unknown error occurred',
+                        type: 'error'
+                    });
+                });
         }
     };
 });
