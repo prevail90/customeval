18a70e517df68722eeff81ffed325278ca5d9cd4
prevail90 <90661674+prevail90@users.noreply.github.com>
2025-05-24 12:33:37 +0000

    Update version.php
    update to v0.1.2

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ $plugin->component = 'mod_customeval';
 $plugin->version = 2024060100;
 $plugin->requires = 2022041900; // Moodle 4.0+
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v0.1.1';
+$plugin->release = 'v0.1.2';
