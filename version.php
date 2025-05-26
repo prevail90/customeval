3afc9eed49051b8627c4198068e21174120b25ef
prevail90 <90661674+prevail90@users.noreply.github.com>
2025-05-26 02:44:16 +0000

    Update version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_customeval';
-$plugin->version = 2025052521;
+$plugin->version = 2025052601;
 $plugin->requires = 2022041900; // Moodle 4.0+
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v0.1.3';
